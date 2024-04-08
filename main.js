2adca97d60587d255242450f21f70355a5671352
LungIris <lung_iris@yahoo.com>
2024-04-08 10:13:04 +0300

    established database connection

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-
+const sqlite3 = require('sqlite3').verbose();
 const { app, BrowserWindow,Menu } = electron;
 
 let mainWindow;
@@ -19,3 +19,18 @@ app.on('ready',function(){
 
 const mainMenuTemplate = [
 ];
+let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to database');
+})
+
+app.on('before-quit', () => {
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Closed the database connection');
+    });
+});
